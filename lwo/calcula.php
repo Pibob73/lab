071527adbc22f6fffbd7034a93a7d6deca290d61
fe <fefe@lab.com>
2022-09-29 21:10:05 +0500

    .

---
+++ lwo/calcula.php
@@ -0,0 +1,31 @@
+<?php
+     function compare($value): bool{
+         $ofnumber = is_numeric($value);
+         return  $ofnumber or $value === '+' or $value === '-';
+     }
+     function separatly($argv): string
+     {
+         $expression = $argv[1];
+         foreach($argv as $key => $value){
+             if($key > 1)
+                 $expression .= ' ' . $value;
+         }
+         return $expression;
+     }
+     function calculator(string $value): string{
+         $massofletters = explode(' ', $value);
+
+         $sum = 0;
+         foreach ($massofletters as $key => $value){
+             if(compare($value)){
+                 if ($massofletters[$key - 1] === '-' and $key != 0)
+                     $value *= -1;
+                 if (is_numeric($value))
+                     $sum += $value;
+             }else return 'invalid value';
+         }
+         return $sum;
+     }
+     $value = separatly($argv);
+     echo calculator($value), "\n";
+?>
