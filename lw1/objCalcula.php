35675adefcf90680a265d5ec8d5234323737d721
fe <fefe@lab.com>
2022-10-10 23:08:15 +0500

    .

--- lw1/objCalcula.php
+++ lw1/objCalcula.php
@@ -1,32 +1,47 @@
 <?php
-class Calculator {
+
+class Calculator
+{
     public $expression;
-    function __Calculator(){
+
+    function __Calculator()
+    {
         $expression = 0;
     }
-    function sum(int $value){
+
+    function sum(int $value)
+    {
         $this->expression += $value;
         return $this;
     }
-    function difference(int $value){
+
+    function difference(int $value)
+    {
         $this->expression -= $value;
         return $this;
     }
-    function multiplication(int $value){
+
+    function multiplication(int $value)
+    {
         $this->expression *= $value;
         return $this;
     }
-    function division(int $value){
-        if($value !== 0) {
+
+    function division(int $value)
+    {
+        if ($value !== 0) {
             $this->expression /= $value;
-        }else{
+        } else {
             $this->expression = 0;
         }
         return $this;
     }
-    function getResult(): string{
+
+    function getResult(): string
+    {
         return $this->expression;
     }
 }
+
 $calc = new Calculator();
-echo $calc->sum(6)->division(0)->difference(2)->multiplication(2)->getResult();
+echo $calc->sum(6)->division(0)->difference(2)->sum(6)->multiplication(2)->getResult();
