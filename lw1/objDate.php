64b594a387dcc6aca0889f0dc833ee92e3b3e4fe
fe <fefe@lab.com>
2022-10-18 00:54:31 +0500

    .

--- lw1/objDate.php
+++ lw1/objDate.php
@@ -2,17 +2,18 @@
 
 class Date
 {
-    private int $year;
-    private int $month;
-    private int $day;
+    public int $year;
+    public int $month;
+    public int $day;
     public int $sumday;
     public string $letter = '.';
 
     public function __construct(int $_day, int $_month, int $_year)
     {
-        if(!$this->examSymbol($_day, $_month)){
+        if($this->examSymbol($_day, $_month)){
             $this->month = 3;
             $this->day = 3;
+            $this->year = 3;
         }else {
             $this->year = $_year;
             $this->month = $_month;
@@ -22,43 +23,44 @@ class Date
         $this->convertToDay($_month, $_year);
     }
 
-    private function convertToDay(int $_month, int $_year)
+    private function convertToDay(int $_month, int $_year): int
     {
-        $h = intval(($_month - 1) / 2);
-        if ($h === 0) $h++;
-        $a = 28;
+        $helper = intval(($_month - 1) / 2);
+        if ($helper === 0) $helper++;
+        $leapYear = 28;
         if ($_year % 4 === 0)
-            $a = 29;
+            $leapYear = 29;
         if ($_month <= 2)
-            $a = 0;
-        $b = 1;
-        if ($_month - 1 === 0) $b--;
-        if (($_month - 1) % 2 === 0 || $h === 1) {
-            $this->sumday += ($h - 1) * 30 + $b * ($h * 31) + $a;
+            $leapYear = 0;
+        $coefficient = 1;
+        if ($_month - 1 === 0) $coefficient--;
+        if (($_month - 1) % 2 === 0 || $helper === 1) {
+            $this->sumday += ($helper - 1) * 30 + $coefficient * ($helper * 31) + $leapYear;
         } else
-            $this->sumday += ($h - 1) * 30 + ($h + 1) * 31 + $a;
+            $this->sumday += ($helper - 1) * 30 + ($helper + 1) * 31 + $leapYear;
         $this->sumday += intval(($_year - 1) / 4) * 366;
         $this->sumday += (($_year - 1) - intval(($_year - 1) / 4)) * 365;
+        return 0;
     }
 
     private function convertToDate(int $num): array
     {
         $massDate = ['day' => 0, 'month' => 0, 'year' => 0];
-        $a = intval($this->year / 4);
-        $num -= 366 * $a;
-        if ($num % 365 !== 0) $a++;
-        $massDate['year'] = intval(($num / 365) + $a);
-        $b = 0;
-        $c = 0;
+        $leapYear = intval($this->year / 4);
+        $num -= 366 * $leapYear;
+        if ($num % 365 !== 0) $leapYear++;
+        $massDate['year'] = intval(($num / 365) + $leapYear);
+        $difference = 0;
+        $differenceFrom = 0;
         if ($num % 365 > 59) {
-            $b = 28;
-            $c = 1;
+            $difference = 28;
+            $differenceFrom = 1;
         }
         if ((($num % 365)) < 31)
             $massDate['month'] = 1;
         else
-            $massDate['month'] = intval(((($num % 365) - $b) / 31) + $c);
-        $massDate['day'] = (($num % 365) - $b) % 31;
+            $massDate['month'] = intval(((($num % 365) - $difference) / 31) + $differenceFrom);
+        $massDate['day'] = (($num % 365) - $difference) % 31;
        return $massDate;
     }
 
@@ -69,9 +71,7 @@ class Date
 
     private function examinationForLetter(string $_letter): bool
     {
-        if ($_letter === 'ru' || $_letter === 'en')
-            return true;
-        return false;
+        return $_letter === 'ru' || $_letter === 'en';
     }
 
     public function diffDay(self $_date): int
@@ -114,17 +114,15 @@ class Date
     public function getDate(): string
     {
         $massDate = ['day' => $this->day, 'month' => $this->month, 'year' => $this->year];
-        $date = implode($this->letter, $massDate);
-        return $date;
+        return implode($this->letter, $massDate);
     }
 }
 
-$date = new Date(4, 2,4 );
-$date2 = new Date(1, 2, 5);
+$date = new Date(1, 2,1 );
+$date2 = new Date(1, 2, 2);
 echo $date->sumday."\n";
 echo $date->format('en')."\n";
 echo $date->getDate()."\n";
 echo $date->diffDay($date2)."\n";
-echo $date2->minusDay(5)."\n";
+echo $date->minusDay(5)."\n";
 echo $date->getDateOfWeek()."\n";
-echo $date->letter."\n";
