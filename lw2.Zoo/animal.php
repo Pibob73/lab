b99865286772246e9c37e778b8932dac1002fc39
fe <fefe@lab.com>
2022-12-22 17:31:30 +0500

    lw2.Zoo/omnivore.php

--- lw2.Zoo/animal.php
+++ lw2.Zoo/animal.php
@@ -1,7 +1,10 @@
 <?php
-abstract class animal{
+
+abstract class animal
+{
     protected string $animal;
     protected string $placeOfResidence;
     protected string $Walk;
+
     protected abstract function meal(int $weight): string;
 }
--- lw2.Zoo/herbivore.php
+++ lw2.Zoo/herbivore.php
@@ -1,12 +1,17 @@
 <?php
 require_once 'animal.php';
-class herbivore extends animal{
-    function __construct(string $animal, string $walk, string $placeOfResidence){
+
+class herbivore extends animal
+{
+    function __construct(string $animal, string $walk, string $placeOfResidence)
+    {
         $this->animal = $animal;
         $this->placeOfResidence = $placeOfResidence;
         $this->Walk = $walk;
     }
-    function meal(int $weight): string{
-        return $this->animal.' needs '.($weight / 4).' kg of hay'."\n";
+
+    function meal(int $weight): string
+    {
+        return $this->animal . ' needs ' . ($weight / 4) . ' kg of hay' . "\n";
     }
 }
