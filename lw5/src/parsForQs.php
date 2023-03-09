f76e30c35ede57df5d67996a92778f5c59d8d8df
fe <fefe@lab.com>
2023-03-10 00:03:43 +0300

    lw5

--- lw5/src/parsForQs.php
+++ lw5/src/parsForQs.php
@@ -1,4 +1,16 @@
 <?php
-        $ch = curl_init('https://favqs.com/api/');
-        echo curl_exec($ch);
-        curl_close($ch);
+$url = 'https://favqs.com/api/qotd/';
+
+$headers = [];
+
+$curl = curl_init(); // создаем экземпляр curl
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_VERBOSE, 0);
+curl_setopt($curl, CURLOPT_POST, false);
+curl_setopt($curl, CURLOPT_URL, $url);
+$result = curl_exec($curl);
+echo json_decode($result)->{'quote'}->{'body'};
+curl_close($curl);
